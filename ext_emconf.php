37a707168c54e4809a4fc5dd4125ccf4f9b0e45a
chris <typo3@mediadreams.org>
2025-12-08 09:29:25 +0100

    [!!!][TASK] do not allow TYPO3 v11 anymore but TYPO3 v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,8 +11,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
-            'scheduler' => '11.5.0-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
+            'scheduler' => '12.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
